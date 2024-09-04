c86e883fbcc5be40b39e299802b6a518b2f4736e
erdey <erdeysyoum@gmail.com>
2024-09-04 14:42:43 +0000

    task 2

---
+++ controllers/AppController.js
@@ -0,0 +1,29 @@
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+class AppController {
+  /**
+   * return
+   * { "redis": true, "db": true } with a status code 200
+   */
+  static getStatus(request, response) {
+    const status = {
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    };
+    response.status(200).send(status);
+  }
+
+  /**
+   * return the number of users and files in DB:
+   */
+  static async getStats(request, response) {
+    const stats = {
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles(),
+    };
+    response.status(200).send(stats);
+  }
+}
+
+export default AppController;
